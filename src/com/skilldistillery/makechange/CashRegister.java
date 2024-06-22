05c70a4a722d2d625e63019fb7288b54f3d2a18c
Adrian <adrian.vidal9589@gmail.com>
2024-06-21 19:52:10 -0600

    Ask for user input

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -8,9 +8,9 @@ public class CashRegister {
 
 		Scanner keyboard = new Scanner(System.in);
 		System.out.print("Please enter the price of your item: ");
-		int userInput = keyboard.nextInt();
-		System.out.print("Please enter the amount of money you will be using: ");
-		int userInput2 = keyboard.nextInt();
+		double userInput = keyboard.nextInt();
+		System.out.print("Please enter how much money you will be using: ");
+		double userInput2 = keyboard.nextInt();
 
 		keyboard.close();
 	}
